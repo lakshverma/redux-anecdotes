9685887677bbcd588dc1982fa0b8aa2b0c48842e
Lakshay Verma <lakshverma23@gmail.com>
2021-11-30 20:10:17 +0530

    fix notification bug so that after multiple actions in a row, notification for last action is displayed correctly

--- src/reducers/notificationReducer.js
+++ src/reducers/notificationReducer.js
@@ -1,10 +1,20 @@
+// Keeping track of setTimeout ensures that if notification is triggered multiple times in a row,
+// we cancel the removal of the previous notification and notification for the current action is displayed for the complete expected duration
+//  instead of getting cleared earlier than expected (because of an earlier setTimeout call for a previous action).
+let previousTimeout = null;
+
 export const setNotification = (message, seconds) => {
   return async (dispatch) => {
+    if (previousTimeout) {
+        console.log(previousTimeout)
+        clearTimeout(previousTimeout)
+    }
+
     dispatch({
       type: "SET_NOTIFICATION",
       data: { message },
     });
-    setTimeout(() => {
+    previousTimeout = setTimeout(() => {
       dispatch({
         type: "REMOVE_NOTIFICATION",
       });
